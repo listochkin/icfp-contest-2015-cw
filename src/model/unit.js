e4129f7e35ad25ca8e0cdd962789467167740823
Andrei T <andrei.tsygankov@gmail.com>
2015-08-07 19:39:43 +0300

    Unit.move works

--- src/model/unit.js
+++ src/model/unit.js
@@ -4,8 +4,37 @@ class Unit {
     this.cells = cells;
   }
   move (direction) {
-    // TODO:
-    return new Unit();
+
+    var deltaXOdd = 0, deltaXEven = 0, deltaY = 0;
+    switch(direction) {
+        case 'SW':
+          deltaXOdd = 0;
+          deltaXEven = -1;
+          deltaY = 1;
+          break;
+        case 'SE':
+          deltaXOdd = 1;
+          deltaXEven = 0;
+          deltaY = 1;
+          break;
+        case 'W':
+          deltaXOdd = -1;
+          deltaXEven = -1;
+          break;
+        case 'E':
+          deltaXOdd = 1;
+          deltaXEven = 1;
+          break;
+    }
+    var pivot = { x: this.pivot.x + (this.pivot.y%2 ? deltaXOdd : deltaXEven), y: this.pivot.y + deltaY};
+    var cells = [];
+
+    this.cells.forEach(function(item, i, arr) {
+      cells.push({ x: item.x + ((item.y%2) ? deltaXOdd : deltaXEven), y: item.y + deltaY} );
+    });
+
+
+    return new Unit(pivot, cells);
   }
   rotate (direction) {
     // TODO:
