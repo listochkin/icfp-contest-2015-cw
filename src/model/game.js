1cb68fdb10665b4c0cfd198fce3d2dddd7269a30
dmitry_b <dbochor@gmail.com>
2015-08-07 19:55:32 +0300

    check valid position

--- src/model/game.js
+++ src/model/game.js
@@ -1,7 +1,30 @@
 class Game {
   isValidPosition (board, unit) {
-    // todo
+
+    let valid = true;
+
+    for (var i = 0; valid && i < unit.members.length; i++) {
+
+      var member = {
+        x: unit.pivot.x + unit.members[i].x,
+        y: unit.pivot.y + unit.members[i].y
+      }
+
+      // check that member is within board bounds
+      if (member.x < 0 || member.x >= board.width) {
+        return false;
+      }
+
+      if (member.y < 0 || member.y >= board.height) {
+        return false;
+      }
+
+      // check that member is on free board cell
+      valid = valid && !board.get(member.x, member.y);
+    }
+    return valid;
   }
+
   lock (board, unit) {
     unit.fillBoard(board);
   }
