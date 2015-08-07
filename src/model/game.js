7841fb6e2c5ada468fe93e4bb84a6997ae66baf5
e-vasyl <e.vasyl@gmail.com>
2015-08-07 19:17:51 +0300

    rename unit cells to members

--- src/model/game.js
+++ src/model/game.js
@@ -3,9 +3,7 @@ class Game {
     // todo
   }
   lock (board, unit) {
-    for (var i = 0; i < unit.cells.length; i++) {
-      board.fill(unit.cells[i].x, unit.cells[i].y);
-    }
+    unit.fillBoard(board);
   }
 }
 
